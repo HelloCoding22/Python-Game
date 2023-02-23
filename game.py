d845b26526821df0ae20fc48e9901d6fe190c3e1
SBA <aquinasogames@gmail.com>
2023-02-23 10:24:43 +0900

    add my text

--- game.py
+++ game.py
@@ -1,35 +1,33 @@
-#소스 코드 수정 중
+# 수정했습니다.
+# num1이 2일 때의 경우를 추가하였습니다.
 import random
 
 num1 = int(input("하나를 선택하세요 : 가위(0), 바위(1), 보(2) :"))
 num2 = random.randrange(0, 3)
-
 if num2 == 0:
-    print("컴퓨터는 가위를 냈습니다.")
+print("컴퓨터는 가위를 냈습니다.")
 elif num2 == 1:
-    print("컴퓨터는 바위를 냈습니다.")
+print("컴퓨터는 바위를 냈습니다.")
 else:
-    print("컴퓨터는 보를 냈습니다.")
-
-
+print("컴퓨터는 보를 냈습니다.")
 if num1 == 0:
-    if num2 == 1:
-        print("컴퓨터가 이겼습니다")
-    elif num2 == 2:
-        print("당신이 이겼습니다.")
-    else:
-        print("비겼습니다.")
-elif num1 == 1:
-    if num2 == 0:
-        print("당신이 이겼습니다.")
-    elif num2 == 2:
-        print("컴퓨터가 이겼습니다.")
-    else:
-        print("비겼습니다.")
+if num2 == 1:
+print("컴퓨터가 이겼습니다")
+elif num2 == 2:
+print("당신이 이겼습니다.")
 else:
-    if num2 == 0:
-        print("컴퓨터가 이겼습니다")
-    elif num2 == 1:
-        print("당신이 이겼습니다.")
-    else:
-        print("비겼습니다.")
+print("비겼습니다.")
+elif num1 == 1:
+if num2 == 0:
+print("당신이 이겼습니다.")
+elif num2 == 2:
+print("컴퓨터가 이겼습니다.")
+else:
+print("비겼습니다.")
+elif num1 == 2:
+if num2 == 0:
+print("컴퓨터가 이겼습니다.")
+elif num2 == 1:
+print("당신이 이겼습니다.")
+else:
+print("비겼습니다.")
